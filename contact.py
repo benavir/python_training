bf3de4e133d409c172d60b8f57378802e2399b7d
Yuriy Podporinov <benavir@mail.ru>
2019-08-14 18:38:06 +0300

    Добавлен вспомогательный метод и класс в репозиторий

---
+++ contact.py
@@ -0,0 +1,9 @@
+class Contact:
+
+    def __init__(self, FirstName, MiddleName, LastName, Title, Company, Address):
+        self.FirstName = FirstName
+        self.MiddleName = MiddleName
+        self.LastName = LastName
+        self.Title = Title
+        self.Company = Company
+        self.Address = Address
