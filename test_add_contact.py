d6d491127acb185281aadc6587d1388574d8efee
Yuriy Podporinov <benavir@mail.ru>
2019-08-19 14:05:32 +0300

    Добавление фикстуры для тестов создания контактов и для создания групп

--- test_add_contact.py
+++ test_add_contact.py
@@ -21,3 +21,4 @@ def test_add_empty_contact(app):
     app.login(username="admin", password="secret")
     app.create_contact(Contact(FirstName="", MiddleName="", LastName="", Title="", Company="", Address=""))
     app.logout()
+
