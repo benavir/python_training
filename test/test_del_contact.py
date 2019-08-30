d3b3bf43e948635042f3ad2be7da4a36c3ecfefa
Yuriy Podporinov <benavir@mail.ru>
2019-08-30 14:48:40 +0300

    Исправление ошибки в тесте удаления контакта и удаления группы

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -3,9 +3,8 @@ from model.contact import Contact
 
 def test_delete_first_contact(app):
     app.contact.home_page()
-    if app.contact.count == 0:
+    if app.contact.count() == 0:
         app.contact.create()
         app.contact.fill(Contact(FirstName="Test"))
         app.contact.submit()
-        app.contact.return_to_home_page()
     app.contact.delete()
