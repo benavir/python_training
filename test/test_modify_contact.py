d46f0f06655d8b436bfa7608b8e9bc15d875c040
Yuriy Podporinov <benavir@mail.ru>
2019-08-30 15:08:08 +0300

    Обеспечение выполнения предусловий в тесте модификации контакта и модификации группы

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -4,11 +4,19 @@ from model.contact import Contact
 
 def test_modify_contact_first_name(app):
     app.contact.home_page()
+    if app.contact.count() == 0:
+        app.contact.create()
+        app.contact.fill(Contact(FirstName="Test"))
+        app.contact.submit()
     app.contact.edit()
     app.contact.modify_first_contact(Contact(FirstName="New First name"))
 
 
 def test_modify_contact_middle_name(app):
     app.contact.home_page()
+    if app.contact.count() == 0:
+        app.contact.create()
+        app.contact.fill(Contact(MiddleName="Test"))
+        app.contact.submit()
     app.contact.edit()
     app.contact.modify_first_contact(Contact(MiddleName="New Middle name"))
