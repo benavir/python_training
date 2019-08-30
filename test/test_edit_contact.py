aaf003a187ed49805156cba4abac4cf427b67f37
Yuriy Podporinov <benavir@mail.ru>
2019-08-30 15:01:57 +0300

    Обеспечение выполнения предусловий в тесте редактирования контакта и редактирования группы

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -3,6 +3,10 @@ from model.contact import Contact
 
 def test_edit_first_contact(app):
     app.contact.home_page()
+    if app.contact.count() == 0:
+        app.contact.create()
+        app.contact.fill(Contact(FirstName="Test"))
+        app.contact.submit()
     app.contact.edit()
     app.contact.fill(Contact(FirstName="Qaz", MiddleName="Wsx", LastName="QazWsx", Title="qwerty", Company="QAZqwerty", Address="Qwerty, 123"))
     app.contact.update()
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,6 +2,11 @@ from model.group import Group
 
 
 def test_edit_first_group(app):
+    if app.group.count() == 0:
+        app.group.create()
+        app.group.fill(Group(name="test"))
+        app.group.submit()
+        app.group.open_groups_page()
     app.group.edit()
     app.group.fill(Group(name="QAZ", header="wsx", footer="QAZwsxQAZ"))
     app.group.update()
