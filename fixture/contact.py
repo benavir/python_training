4b552391e05797081cad9d1278a85824ae83cb1c
Yuriy Podporinov <benavir@mail.ru>
2019-09-04 20:26:40 +0300

    исправлен тест удаления контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -45,6 +45,7 @@ class ContactHelper:
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to_alert().accept()
         wd.find_element_by_css_selector("div.msgbox")
+        self.contact_cash = None
 
     def select_first_contact(self):
         wd = self.app.wd
