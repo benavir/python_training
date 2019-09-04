2d59b0fb2b1d57d1ad48e654efc1410cb201996c
Yuriy Podporinov <benavir@mail.ru>
2019-09-04 12:06:18 +0300

    проверки в виде сравнения списков

--- fixture/contact.py
+++ fixture/contact.py
@@ -49,6 +49,7 @@ class ContactHelper:
         # submit deletion
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to_alert().accept()
+        wd.find_element_by_css_selector("div.msgbox")
 
     def select_first_contact(self):
         wd = self.app.wd
