b268c86577c1a56f9f76976168a241f7ee30e381
Yuriy Podporinov <benavir@mail.ru>
2019-09-15 21:45:00 +0300

    предварительный (14 задание)(обратные проверки): выбирается случайный контакт для проверки телефона и электронного адреса на домашней странице и странице контактов

--- test/test_emails.py
+++ test/test_emails.py
@@ -3,19 +3,25 @@ from random import randrange
 
 
 def test_emails_on_home_page(app):
-    equal_emails_on_home_page = app.contact.get_contact_list()
-    index = randrange(len(equal_emails_on_home_page))
+    index = select_random_contact(app)
     contact_from_home_page = app.contact.get_contact_list()[index]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
 
 
-#def test_emails_on_contact_view_page(app):
-#    contact_from_view_page = app.contact.get_contact_from_view_page(0)
-#    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-#    assert contact_from_view_page.email == contact_from_edit_page.email
-#    assert contact_from_view_page.email2 == contact_from_edit_page.email2
-#    assert contact_from_view_page.email3 == contact_from_edit_page.email3
+def select_random_contact(app):
+    equal_emails_on_home_page = app.contact.get_contact_list()
+    index = randrange(len(equal_emails_on_home_page))
+    return index
+
+
+def test_emails_on_contact_view_page(app):
+    index = select_random_contact(app)
+    contact_from_view_page = app.contact.get_contact_from_view_page(index)
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+    assert contact_from_view_page.email == contact_from_edit_page.email
+    assert contact_from_view_page.email2 == contact_from_edit_page.email2
+    assert contact_from_view_page.email3 == contact_from_edit_page.email3
 
 
 def clear(s):
--- test/test_phones.py
+++ test/test_phones.py
@@ -2,21 +2,27 @@ import re
 from random import randrange
 
 
-def test_phones_on_home_page(app):
+def select_random_contact(app):
     equal_phones_on_home_page = app.contact.get_contact_list()
     index = randrange(len(equal_phones_on_home_page))
+    return index
+
+
+def test_phones_on_home_page(app):
+    index = select_random_contact(app)
     contact_from_home_page = app.contact.get_contact_list()[index]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
 
-#def test_phones_on_contact_view_page(app):
-#    contact_from_view_page = app.contact.get_contact_from_view_page(0)
-#    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-#    assert contact_from_view_page.homephone == contact_from_edit_page.homephone
-#    assert contact_from_view_page.mobilephone == contact_from_edit_page.mobilephone
-#    assert contact_from_view_page.workphone == contact_from_edit_page.workphone
-#    assert contact_from_view_page.secondaryphone == contact_from_edit_page.secondaryphone
+def test_phones_on_contact_view_page(app):
+    index = select_random_contact(app)
+    contact_from_view_page = app.contact.get_contact_from_view_page(index)
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+    assert contact_from_view_page.homephone == contact_from_edit_page.homephone
+    assert contact_from_view_page.mobilephone == contact_from_edit_page.mobilephone
+    assert contact_from_view_page.workphone == contact_from_edit_page.workphone
+    assert contact_from_view_page.secondaryphone == contact_from_edit_page.secondaryphone
 
 
 def clear(s):
