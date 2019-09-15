876847d03735aebd2348ae5512563f568969492e
Yuriy Podporinov <benavir@mail.ru>
2019-09-15 22:24:54 +0300

    предварительный (14 задание)(обратные проверки): выбирается случайный контакт для проверки телефона и электронного адреса на домашней странице и странице контактов

--- test/test_emails.py
+++ test/test_emails.py
@@ -2,17 +2,17 @@ import re
 from random import randrange
 
 
+def select_random_contact(app):
+    equal_emails_on_home_page = app.contact.get_contact_list()
+    index = randrange(len(equal_emails_on_home_page))
+    return index
+
+
 def test_emails_on_home_page(app):
     index = select_random_contact(app)
     contact_from_home_page = app.contact.get_contact_list()[index]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
-
-
-def select_random_contact(app):
-    equal_emails_on_home_page = app.contact.get_contact_list()
-    index = randrange(len(equal_emails_on_home_page))
-    return index
 
 
 def test_emails_on_contact_view_page(app):
