d43febdcd63361c9ddcccb7a2b0375b695d75a4b
Yuriy Podporinov <benavir@mail.ru>
2019-09-17 14:42:59 +0300

    использование текущей директориии

--- conftest.py
+++ conftest.py
@@ -30,6 +30,7 @@ def stop(request):
     request.addfinalizer(fin)
     return fixture
 
+
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--target", action="store", default="target.json")
