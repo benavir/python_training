d0075a76bf6af54dc38ab2f8139870c4e19a1711
Yuriy Podporinov <benavir@mail.ru>
2019-09-18 11:29:44 +0300

    Проверка почтовых адресов на не пустое поле. Объединение обоих тестов (проверки емейлов и телефонов) в один

---
+++ test/test_contacts.py
@@ -0,0 +1,44 @@
+import re
+from random import randrange
+
+
+def select_random_contact(app):
+    equal_contacts_on_home_page = app.contact.get_contact_list()
+    index = randrange(len(equal_contacts_on_home_page))
+    return index
+
+
+def test_contacts_on_home_page(app):
+    index = select_random_contact(app)
+    contact_from_home_page = app.contact.get_contact_list()[index]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
+
+
+def test_contacts_on_contact_view_page(app):
+    index = select_random_contact(app)
+    contact_from_view_page = app.contact.get_contact_from_view_page(index)
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+    assert contact_from_view_page.email == contact_from_edit_page.email
+    assert contact_from_view_page.email2 == contact_from_edit_page.email2
+    assert contact_from_view_page.email3 == contact_from_edit_page.email3
+    assert contact_from_view_page.homephone == contact_from_edit_page.homephone
+    assert contact_from_view_page.mobilephone == contact_from_edit_page.mobilephone
+    assert contact_from_view_page.workphone == contact_from_edit_page.workphone
+    assert contact_from_view_page.secondaryphone == contact_from_edit_page.secondaryphone
+
+
+def clear(s):
+    return re.sub("[() -]", "", s)
+
+
+def merge_emails_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "", ([contact.email, contact.email2, contact.email3])))
+
+
+def merge_phones_like_on_home_page(contact):
+    return "\n".join(filter(lambda x: x != "",
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None,
+                                       [contact.homephone, contact.mobilephone, contact.workphone, contact.secondaryphone]))))
