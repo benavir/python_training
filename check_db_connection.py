e5bdc3e2f9f0b1a44d426d13ae85cf98da1c7dd6
Yuriy Podporinov <benavir@mail.ru>
2019-10-08 18:55:00 +0300

    Ограничения или условия отбора загружаемых данных

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,11 +1,22 @@
-import pymysql.cursors
+# import pymysql.cursors
+from fixture.db import DbFixture
 
-connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+
+# connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
 
 try:
+#    groups = db.get_group_list()
+    contacts = db.get_contact_list()
+    for contact in contacts:
+        print(contact)
+    print(len(contacts))
+    '''
     cursor = connection.cursor()
     cursor.execute("select * from group_list")
     for row in cursor.fetchall():
         print(row)
+    '''
 finally:
-    connection.close()
+#    connection.close()
+    db.destroy()
