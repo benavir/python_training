a4af34e1898e7b390c55ec4279bd1912fb4e1ff6
Yuriy Podporinov <benavir@mail.ru>
2019-10-18 15:26:47 +0300

    Задание №22: проверка на добавление контакта в группу

--- test/test_add_contact_to_group.py
+++ test/test_add_contact_to_group.py
@@ -4,7 +4,7 @@ from fixture.orm import ORMFixture
 import random
 
 
-def test_info_contacts_on_home_page(app, db):
+def test_info_contacts_on_home_page(app, db, orm):
     if len(db.get_contact_list()) == 0:
         app.contact.create()
         app.contact.fill(Contact(FirstName="Test", LastName="Test", Address="adress1",
@@ -19,7 +19,21 @@ def test_info_contacts_on_home_page(app, db):
     list_of_contacts = db.get_contact_list()
     list_of_groups = db.get_group_list()
     contact = random.choice(list_of_contacts)
-    group = random.choice(list_of_groups)
+
+    # Проверить, в каких группах контакт не состоит
+    groups_without_contact = []
+    for group in list_of_groups:
+        if not orm.check_if_contact_is_in_group(group, contact):
+            groups_without_contact.append(group)
+    # Если контакт входит во все группы, то создать новую группу
+    if len(groups_without_contact) == 0:
+        app.group.create()
+        app.group.fill(Group(name="test_group"))
+        app.group.submit()
+        app.group.return_to_home_page()
+        groups_without_contact = orm.get_group_list()
+    # Выбрать группу из списка групп без контакта
+    group = random.choice(groups_without_contact)
     app.contact.add_contact_to_group_by_id(contact.id, group.id)
     contacts_in_group_from_group_page = sorted(app.contact.get_contact_list_in_group(), key=Contact.id_or_max)
     db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
