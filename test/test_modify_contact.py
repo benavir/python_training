c9c3e2c0572f1cfffaf7de45561d4b2c5b7aa33b
Yuriy Podporinov <benavir@mail.ru>
2019-11-19 23:27:18 +0300

    jenkins 1

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,6 +1,5 @@
 from model.contact import Contact
 import random
-import time
 
 
 def test_modify_contact_first_name(app, db, json_contacts, check_ui):
@@ -16,7 +15,6 @@ def test_modify_contact_first_name(app, db, json_contacts, check_ui):
     new_contacts = db.get_contact_list()
     # assert len(old_contacts) == len(new_contacts)
     old_contacts[index] = new_contacts[index]
-    time.sleep(7)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
     if check_ui:
         assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
