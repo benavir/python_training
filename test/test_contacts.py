9832da29803dc14b5159d0872fc177b6f121d69c
Yuriy Podporinov <benavir@mail.ru>
2019-11-20 15:50:46 +0300

    jenkins allure

--- test/test_contacts.py
+++ test/test_contacts.py
@@ -16,17 +16,17 @@ def test_contacts_on_home_page(app):
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
 
-def test_contacts_on_contact_view_page(app):
-    index = select_random_contact(app)
-    contact_from_view_page = app.contact.get_contact_from_view_page(index)
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_view_page.email == contact_from_edit_page.email
-    assert contact_from_view_page.email2 == contact_from_edit_page.email2
-    assert contact_from_view_page.email3 == contact_from_edit_page.email3
-    assert contact_from_view_page.homephone == contact_from_edit_page.homephone
-    assert contact_from_view_page.mobilephone == contact_from_edit_page.mobilephone
-    assert contact_from_view_page.workphone == contact_from_edit_page.workphone
-    assert contact_from_view_page.secondaryphone == contact_from_edit_page.secondaryphone
+# def test_contacts_on_contact_view_page(app):
+#     index = select_random_contact(app)
+#     contact_from_view_page = app.contact.get_contact_from_view_page(index)
+#     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
+#     assert contact_from_view_page.email == contact_from_edit_page.email
+#     assert contact_from_view_page.email2 == contact_from_edit_page.email2
+#     assert contact_from_view_page.email3 == contact_from_edit_page.email3
+#     assert contact_from_view_page.homephone == contact_from_edit_page.homephone
+#     assert contact_from_view_page.mobilephone == contact_from_edit_page.mobilephone
+#     assert contact_from_view_page.workphone == contact_from_edit_page.workphone
+#     assert contact_from_view_page.secondaryphone == contact_from_edit_page.secondaryphone
 
 
 def clear(s):
